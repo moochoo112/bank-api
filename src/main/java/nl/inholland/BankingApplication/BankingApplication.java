a148dac60c84beffa63f4c18347e540f9b63a2e8
moochoo112 <43070896+moochoo112@users.noreply.github.com>
2020-05-04 22:15:58 +0200

    banking ding

---
+++ src/main/java/nl/inholland/BankingApplication/BankingApplication.java
@@ -0,0 +1,17 @@
+package nl.inholland.BankingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApplication.class, args);
+		System.out.println("Lekker hij werkt joepie!");
+		System.out.println("Sam is the king!!");
+	}
+
+
+
+}
